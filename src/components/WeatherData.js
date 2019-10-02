7038bf78bc8d36a2b721458d67bfde753138b48f
krif07 <krif07@gmail.com>
2019-10-02 10:46:29 -0500

    mejoras

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/weathers';
 
 const WeatherData = () =>(
     <div>
-        <WeatherTemperature temperature={20} weatherState={'fog'}/>
+        <WeatherTemperature 
+            temperature={22} 
+            weatherState={WINDY}
+        />
         <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>        
     </div>
 );
